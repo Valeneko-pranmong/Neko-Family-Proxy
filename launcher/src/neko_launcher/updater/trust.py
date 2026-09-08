10825bfe3b65fd67ba68524b35d31079db870054
Vale3neko <zalovenext@gmail.com>
2026-09-08 09:23:51 +0700

    feat(update): provision production release trust

--- launcher/src/neko_launcher/updater/trust.py
+++ launcher/src/neko_launcher/updater/trust.py
@@ -1,3 +1,5 @@
 from typing import Mapping
 
-PRODUCTION_RELEASE_PUBLIC_KEYS: Mapping[str, bytes] = {}
+PRODUCTION_RELEASE_PUBLIC_KEYS: Mapping[str, bytes] = {
+    "neko-update-prod-1": bytes.fromhex("c98941b472a01967a54e4f1cb215f71a479cf187ebdd0fa37f4ee9a5693fe9cf"),
+}
